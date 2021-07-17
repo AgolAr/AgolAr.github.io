1c67336d059a9fb21e89f12518c8518bd798cc0e
AgolAr <aaron.agola@gmail.com>
2021-07-17 03:44:41 +0100

    New files added

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import logo1 from './DockerLogo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" /><br />
+	<img src={logo1} className="App-logo1" alt="logo1" />
+        <p>
+          Hello World!!<br/>Please Edit the <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
